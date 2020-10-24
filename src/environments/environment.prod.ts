5fbbdcc9b1c717d99047b26c6464dec37f2ca2a9
César Gonzalez <cedgo1997@gmail.com>
2020-10-24 14:27:57 -0400

    Firestore imports added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB2Cor9y4B016saZr2_K7Y1Zhb8UwvaUso",
+    authDomain: "inandout-app.firebaseapp.com",
+    databaseURL: "https://inandout-app.firebaseio.com",
+    projectId: "inandout-app",
+    storageBucket: "inandout-app.appspot.com",
+    messagingSenderId: "670052225441",
+    appId: "1:670052225441:web:7bca883848a95eba91bbe0",
+    measurementId: "G-7GY66SSJWY",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB2Cor9y4B016saZr2_K7Y1Zhb8UwvaUso",
+    authDomain: "inandout-app.firebaseapp.com",
+    databaseURL: "https://inandout-app.firebaseio.com",
+    projectId: "inandout-app",
+    storageBucket: "inandout-app.appspot.com",
+    messagingSenderId: "670052225441",
+    appId: "1:670052225441:web:7bca883848a95eba91bbe0",
+    measurementId: "G-7GY66SSJWY",
+  },
 };
 
 /*
