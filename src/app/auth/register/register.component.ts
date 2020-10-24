f1a9920213842bbc4ab3bf2dae08141562484396
César Gonzalez <cedgo1997@gmail.com>
2020-10-23 23:46:49 -0400

    Reactive Forms to regster component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styles: []
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styles: [],
 })
 export class RegisterComponent implements OnInit {
+  formGroup: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.formGroup = this.fb.group({
+      usuario: ["", Validators.required],
+      correo: ["", Validators.required, Validators.email],
+      contrasena: ["", Validators.required],
+    });
   }
 
+  crearUsuario() {
+    console.log(this.formGroup);
+    console.log(this.formGroup.valid);
+    console.log(this.formGroup.value);
+  }
 }
