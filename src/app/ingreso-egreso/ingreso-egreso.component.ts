dc8d2c17c84553bdc36749cc55980cffebb9dd4a
César Gonzalez <cedgo1997@gmail.com>
2020-10-25 16:37:23 -0400

    fix ingresoEgreso form

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-ingreso-egreso',
-  templateUrl: './ingreso-egreso.component.html',
-  styles: []
+  selector: "app-ingreso-egreso",
+  templateUrl: "./ingreso-egreso.component.html",
+  styles: [],
 })
 export class IngresoEgresoComponent implements OnInit {
+  ingresoEgresoForm: FormGroup;
+  tipo: string = "ingreso";
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.ingresoEgresoForm = this.fb.group({
+      descripcion: ["", Validators.required],
+      monto: ["", Validators.required],
+    });
   }
-
+  guardar() {
+    if (this.ingresoEgresoForm.invalid) {
+      return;
+    }
+    console.log(this.ingresoEgresoForm.value);
+  }
 }
