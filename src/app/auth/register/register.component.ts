3dcc95854277069c1e71a3222dffdcbf769224cb
César Gonzalez <cedgo1997@gmail.com>
2020-10-24 23:05:11 -0400

    register loading dispatch ready

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { Subscription } from "rxjs";
+import { AppState } from "src/app/app.reducer";
 import { AuthService } from "src/app/services/auth.service";
+import { isLoading, stopLoading } from "src/app/shared/ui.actions";
 import Swal from "sweetalert2";
 
 @Component({
@@ -9,13 +13,16 @@ import Swal from "sweetalert2";
   templateUrl: "./register.component.html",
   styles: [],
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
   formGroup: FormGroup;
+  cargando: boolean = false;
+  uiSubs: Subscription;
 
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private store: Store<AppState>
   ) {}
 
   ngOnInit() {
@@ -24,6 +31,15 @@ export class RegisterComponent implements OnInit {
       correo: ["", [Validators.required, Validators.email]],
       contrasena: ["", Validators.required],
     });
+
+    this.uiSubs = this.store.select("ui").subscribe((ui) => {
+      this.cargando = ui.isLoading;
+      console.log("cargando subs en register");
+    });
+  }
+
+  ngOnDestroy() {
+    this.uiSubs.unsubscribe();
   }
 
   crearUsuario() {
@@ -31,27 +47,31 @@ export class RegisterComponent implements OnInit {
       return;
     }
 
-    Swal.fire({
+    this.store.dispatch(isLoading());
+
+    /* Swal.fire({
       title: "¡Cargando!",
       willOpen: () => {
         Swal.showLoading();
       },
-    });
+    }); */
 
     const { usuario, correo, contrasena } = this.formGroup.value;
     this.authService
       .crearUsuario(usuario, correo, contrasena)
       .then((credenciales) => {
-        Swal.close();
+        /* Swal.close(); */
+        this.store.dispatch(stopLoading());
         console.log(credenciales);
         this.router.navigate(["/"]);
       })
       .catch((err) => {
-        Swal.fire({
+        this.store.dispatch(stopLoading());
+        /* Swal.fire({
           icon: "error",
           title: "Oops...",
           text: err.message,
-        });
+        }); */
       });
   }
 }
