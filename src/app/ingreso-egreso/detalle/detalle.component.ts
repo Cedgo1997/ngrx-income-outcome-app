89b1311adf28341a4d751657166715343a88604d
César Gonzalez <cedgo1997@gmail.com>
2020-10-25 23:11:48 -0400

    Detalles working

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { Store } from "@ngrx/store";
+import { Subscription } from "rxjs";
 import { AppState } from "src/app/app.reducer";
 import { IngresoEgreso } from "src/app/models/ingreso-egreso.model";
 
@@ -8,16 +9,25 @@ import { IngresoEgreso } from "src/app/models/ingreso-egreso.model";
   templateUrl: "./detalle.component.html",
   styles: [],
 })
-export class DetalleComponent implements OnInit {
+export class DetalleComponent implements OnInit, OnDestroy {
   ingresosEgresos: IngresoEgreso[] = [];
+  ingresosSubs: Subscription;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
-    this.store
+    this.ingresosSubs = this.store
       .select("ingresoEgreso")
       .subscribe(
         (ingresoEgreso) => (this.ingresosEgresos = ingresoEgreso.items)
       );
+  }
+
+  ngOnDestroy() {
+    this.ingresosSubs.unsubscribe();
+  }
+
+  borrar(uid: string) {
+    console.log(uid);
   }
 }
