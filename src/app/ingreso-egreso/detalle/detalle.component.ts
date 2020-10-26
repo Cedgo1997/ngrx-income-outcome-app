8899919c0a63be4f474c2e746de4e02374284671
César Gonzalez <cedgo1997@gmail.com>
2020-10-25 22:54:52 -0400

    Get items on detalle components

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { AppState } from "src/app/app.reducer";
+import { IngresoEgreso } from "src/app/models/ingreso-egreso.model";
 
 @Component({
-  selector: 'app-detalle',
-  templateUrl: './detalle.component.html',
-  styles: []
+  selector: "app-detalle",
+  templateUrl: "./detalle.component.html",
+  styles: [],
 })
 export class DetalleComponent implements OnInit {
+  ingresosEgresos: IngresoEgreso[] = [];
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
+    this.store
+      .select("ingresoEgreso")
+      .subscribe(
+        (ingresoEgreso) => (this.ingresosEgresos = ingresoEgreso.items)
+      );
   }
-
 }
